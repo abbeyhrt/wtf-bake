addb28eafec7186dc5c1f910e001d9fa60514217
Abbey Hart <abbeyhrt@gmail.com>
2017-12-21 17:01:45 -0500

    Style changes

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -2,4 +2,7 @@ window.addEventListener('keydown', e => {
   if (e.keyCode === 13) {
     window.location.replace('/recipes/random');
   }
+  if (e.keyCode === 32) {
+    window.location.replace('/recipes/random');
+  }
 });
